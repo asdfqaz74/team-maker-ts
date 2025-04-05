6cc715fea75d407f953a4247898543e3f8aeff2c
KangJS <asdfqaz1996@naver.com>
2025-04-05 12:45:15 +0900

    유저 정보를 가져오고 생성하는 API 추가

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,7 +1,27 @@
 // /app/api/test/route.js
 import { connectDB } from "@/lib/mongoose";
+import User from "@/models/User";
 
 export async function GET() {
   await connectDB();
-  return Response.json({ message: "✅ DB 연결 성공!" });
+
+  const users = await User.find();
+  return Response.json({ message: "GET 요청 성공", users });
+}
+
+export async function POST(request) {
+  try {
+    console.log("POST request received");
+    await connectDB();
+
+    const body = await request.json();
+    const user = await User.create(body);
+    return Response.json(user);
+  } catch (error) {
+    console.error("POST 요청 처리 중 오류:", error);
+    return Response.json(
+      { error: "데이터를 저장하는 중 오류가 발생했습니다." },
+      { status: 500 }
+    );
+  }
 }
---
+++ app/api/user/route.js
@@ -0,0 +1,19 @@
+import { connectDB } from "@/lib/mongoose";
+import User from "@/models/User";
+
+// 유저 정보를 가져오는 API
+export async function GET() {
+  await connectDB();
+
+  const users = await User.find();
+  return Response.json(users);
+}
+
+// 유저 정보를 생성하는 API
+// 유저 정보 생성할 때는 name, nickName 이 필수값입니다.
+export async function POST(request) {
+  await connectDB();
+  const body = await request.json();
+  const user = await User.create(body);
+  return Response.json(user, { status: 201 });
+}
