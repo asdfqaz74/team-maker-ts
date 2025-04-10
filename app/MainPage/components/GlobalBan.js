724c51abfd1ab3ea71961beb262b848e72b34416
KangJS <asdfqaz1996@naver.com>
2025-04-11 03:31:02 +0900

    로딩 이미지 스타일 수정 및 챔피언 리스트 레이아웃 개선

--- app/MainPage/components/GlobalBan.js
+++ app/MainPage/components/GlobalBan.js
@@ -52,14 +52,14 @@ export default function GlobalBan() {
               key={idx}
               className="bg-[#1B4A85] w-[10rem] h-[10rem] rounded-2xl flex items-center justify-center"
             >
-              <div className="w-[130px] h-[160px] bg-gray-700 rounded animate-pulse" />
+              <div className="w-[8.125rem] h-[14.75rem] bg-gray-700 rounded animate-pulse" />
             </li>
           ))}
         </ul>
       )}
 
       {!isLoading && (
-        <ul className="grid grid-cols-5 gap-2 mt-20">
+        <ul className="flex justify-between gap-2 mt-20">
           {champions.map((champion, idx) => (
             <li
               key={idx}
@@ -69,10 +69,10 @@ export default function GlobalBan() {
                 src={`/images/champions/loading/${champion.en_name}.jpg`}
                 alt={champion.name}
                 width={130}
-                height={160}
+                height={236.13}
                 className="rounded"
               />
-              <span>{champion.name}</span>
+              <span className="mt-2 font-semibold">{champion.name}</span>
             </li>
           ))}
         </ul>
