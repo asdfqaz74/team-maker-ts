453499aa7cf7afb8e6a7cff82d80312a11fbe93f
KangJS <asdfqaz1996@naver.com>
2025-04-11 02:30:42 +0900

    이미지 설정 개선: remotePatterns 추가 및 코드 구조 정리

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["imgur.com"] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+    minimumCacheTTL: 60 * 60 * 24 * 7,
+    domains: ["imgur.com"],
+  },
 };
 
 export default nextConfig;
