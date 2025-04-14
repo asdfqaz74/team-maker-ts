b062bc1eb884b23617c37dc6e327cf8fe0ed5628
KangJS <asdfqaz1996@naver.com>
2025-04-14 20:35:31 +0900

    챔피언 팔레트 컴포넌트를 다이얼로그로 감싸고, 페이지에서 모달 열기 기능 추가

--- app/components/ChampionPalette.js
+++ app/components/ChampionPalette.js
@@ -1,10 +1,11 @@
 "use client";
 import { fetchChampionList } from "@/lib/api/fetchChampionList";
+import { Dialog } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useState } from "react";
 
-export default function ChampionPalette() {
+export default function ChampionPalette({ open, onClose }) {
   const [searchChampion, setSearchChampion] = useState("");
   const [selected, setSelected] = useState([]);
   const {
@@ -33,52 +34,54 @@ export default function ChampionPalette() {
   if (isError) return <div>{error.message}</div>;
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <input
-        type="text"
-        className="bg-[#D9D9D9] placeholder-gray-500 p-2 text-gray-950 w-1/2"
-        placeholder="챔피언 검색"
-        value={searchChampion}
-        onChange={(e) => setSearchChampion(e.target.value)}
-      />
-      <div className="grid grid-cols-6 gap-2">
-        {filteredChampionList.map((champ) => (
-          <button
-            key={champ.id}
-            onClick={() => toggleChampion(champ.id)}
-            className={`border-2 rounded flex flex-col items-center justify-center ${
-              selected.includes(champ.id)
-                ? "border-blue-500"
-                : "border-transparent"
-            }`}
-          >
-            <Image
-              src={champ.image}
-              alt={champ.name}
-              className="object-cover cursor-pointer"
-              width={64}
-              height={64}
-            />
-            <p className="text-xs text-white">{champ.name}</p>
-          </button>
-        ))}
-      </div>
-      <div className="flex gap-2 flex-wrap">
-        {selected.map((id) => {
-          const champ = championList.find((c) => c.id === id);
-          return (
-            <div
-              key={id}
-              className="text-sm px-2 py-1 bg-blue-600 text-white rounded"
+    <Dialog open={open} onClose={onClose}>
+      <div className="flex flex-col justify-center items-center">
+        <input
+          type="text"
+          className="bg-[#D9D9D9] placeholder-gray-500 p-2 text-gray-950 w-1/2"
+          placeholder="챔피언 검색"
+          value={searchChampion}
+          onChange={(e) => setSearchChampion(e.target.value)}
+        />
+        <div className="grid grid-cols-6 gap-2">
+          {filteredChampionList.map((champ) => (
+            <button
+              key={champ.id}
+              onClick={() => toggleChampion(champ.id)}
+              className={`border-2 rounded flex flex-col items-center justify-center ${
+                selected.includes(champ.id)
+                  ? "border-blue-500"
+                  : "border-transparent"
+              }`}
             >
-              {champ?.name}
-            </div>
-          );
-        })}
+              <Image
+                src={champ.image}
+                alt={champ.name}
+                className="object-cover cursor-pointer"
+                width={64}
+                height={64}
+              />
+              <p className="text-xs text-white">{champ.name}</p>
+            </button>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-wrap">
+          {selected.map((id) => {
+            const champ = championList.find((c) => c.id === id);
+            return (
+              <div
+                key={id}
+                className="text-sm px-2 py-1 bg-blue-600 text-white rounded"
+              >
+                {champ?.name}
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex">
+          <button className="cursor-pointer">설정</button>
+        </div>
       </div>
-      <div className="flex">
-        <button className="cursor-pointer">설정</button>
-      </div>
-    </div>
+    </Dialog>
   );
 }
--- app/fearless/page.js
+++ app/fearless/page.js
@@ -1,9 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import ChampionPalette from "../components/ChampionPalette";
 
 export default function Page() {
+  const [openModal, setOpenModal] = useState(false);
+  console.log(openModal);
   return (
     <div>
-      <ChampionPalette />
+      <button
+        className="cursor-pointer bg-gray-800"
+        onClick={() => setOpenModal(true)}
+      >
+        설정하기
+      </button>
+      <ChampionPalette open={openModal} onClose={() => setOpenModal(false)} />
     </div>
   );
 }
