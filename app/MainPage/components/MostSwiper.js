f50082f5b3690b300021141e556e588147805fee
KangJS <asdfqaz1996@naver.com>
2025-04-15 19:45:45 +0900

    MostSwiper 컴포넌트에 커스텀 네비게이션 버튼 추가 및 UI 개선

--- app/MainPage/components/MostSwiper.js
+++ app/MainPage/components/MostSwiper.js
@@ -13,10 +13,10 @@ export default function MostSwiper({ champions }) {
   const background = champions?.[secondIndex]?.logo || "";
 
   return (
-    <div className="relative w-full h-[37.5rem] rounded-2xl overflow-hidden">
+    <div className="relative w-full h-[37.5rem] rounded-2xl">
       {/* 배경 이미지 */}
       <div
-        className="absolute inset-0 bg-black z-0 transition-all duration-300"
+        className="absolute inset-0 bg-black z-0 transition-all duration-300 rounded-xl"
         style={{
           backgroundImage: `url(${background})`,
           backgroundSize: "cover",
@@ -24,6 +24,8 @@ export default function MostSwiper({ champions }) {
           opacity: 0.5,
         }}
       />
+
+      {/* 타이틀 */}
       <Image
         src="/images/MainPage/MostLogo.png"
         alt="모스트 챔피언 5"
@@ -33,15 +35,20 @@ export default function MostSwiper({ champions }) {
       />
 
       {/* Swiper 콘텐츠 */}
+
       <div className="relative w-full h-full z-10 py-10 px-5">
         <Swiper
           modules={[Navigation, Autoplay]}
-          navigation
+          navigation={{
+            nextEl: ".swiper-custom-next",
+            prevEl: ".swiper-custom-prev",
+          }}
           spaceBetween={30}
           slidesPerView={4}
           loop={true}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
           onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+          speed={3000}
           className="w-full h-full"
         >
           {champions.map((champion, idx) => {
@@ -58,7 +65,7 @@ export default function MostSwiper({ champions }) {
                 >
                   <div
                     className={`absolute w-full h-full rounded-2xl overflow-hidden
-                    }`}
+                      }`}
                   >
                     <Image
                       src={champion.image}
@@ -70,10 +77,39 @@ export default function MostSwiper({ champions }) {
                     />
                   </div>
                 </div>
+                {isSecondSlide && (
+                  <div className="absolute top-0 -right-[31.25rem]">
+                    <div className="flex flex-col gap-4">
+                      <p className="text-4xl font-bold">{champion.name}</p>
+                      <div className="flex gap-4 items-end">
+                        <p className="text-xl font-semibold">총 게임 수</p>
+                        <p>{champion.count} 게임</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </SwiperSlide>
             );
           })}
         </Swiper>
+      </div>
+      {/* 커스텀 버튼 */}
+      <div className="swiper-custom-prev absolute -left-20 top-1/2 -translate-y-1/2 z-30 w-[60px] h-[60px] cursor-pointer">
+        <Image
+          src="/images/MainPage/Left circle.png"
+          alt="이전 슬라이드"
+          fill
+          className="object-contain"
+        />
+      </div>
+
+      <div className="swiper-custom-next absolute -right-20 top-1/2 -translate-y-1/2 z-30 w-[50px] h-[50px] cursor-pointer rotate-180">
+        <Image
+          src="/images/MainPage/Left circle.png"
+          alt="다음 슬라이드"
+          fill
+          className="object-contain"
+        />
       </div>
     </div>
   );
