e3883cdf8f7f9d37acbdcf4d2d16b931aa77aec4
KangJS <asdfqaz1996@naver.com>
2025-04-18 21:12:52 +0900

    UI 개선: PlayerDB 컴포넌트에 토큰 확인 로직 추가 및 로그인 페이지 리다이렉트 기능 구현

--- app/playerDB/page.js
+++ app/playerDB/page.js
@@ -1,8 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import PlayerInfo from "./components/PlayerInfo";
+import { getToken } from "@/utils/getToken";
+import { useEffect, useState } from "react";
 
 export default function PlayerDB() {
+  const router = useRouter();
+  const [tokenCheck, setTokenCheck] = useState(false);
+
+  // 토큰이 있는지 확인, 없으면 로그인 페이지로 이동
+  useEffect(() => {
+    const token = getToken();
+
+    if (!token) {
+      router.push("/auth/login");
+    } else {
+      setTokenCheck(true);
+    }
+  }, [router]);
+
+  if (!tokenCheck) return null;
+
   return (
     <div className="px-60 py-20">
       <PlayerInfo />
