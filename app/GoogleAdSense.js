bc27da336f1242776cce3098c2ffe9c3d43e4922
KangJS <asdfqaz1996@naver.com>
2025-04-19 09:53:33 +0900

    구글 애드센스 스크립트의 클라이언트 ID를 하드코딩된 값으로 변경

--- app/GoogleAdSense.js
+++ app/GoogleAdSense.js
@@ -1,12 +1,10 @@
 import Script from "next/script";
 
-const PID = process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID;
-
 export default function GoogleAdSense() {
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${PID}`}
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1539093914229562"
       crossOrigin="anonymous"
       strategy="lazyOnload"
     />
