10a118f6213c41e6cf585d9321f646ca8ec548f8
KangJS <asdfqaz1996@naver.com>
2025-04-19 09:49:00 +0900

    구글 애드센스 스크립트의 클라이언트 ID를 환경 변수로 변경

--- app/GoogleAdSense.js
+++ app/GoogleAdSense.js
@@ -1,10 +1,12 @@
 import Script from "next/script";
 
+const PID = process.env.PID;
+
 export default function GoogleAdSense() {
   return (
     <Script
       async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1539093914229562"
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${PID}`}
       crossOrigin="anonymous"
       strategy="lazyOnload"
     />
