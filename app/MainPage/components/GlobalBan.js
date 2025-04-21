3f5e8aec7fcc3e0f3de11e7f7a106445a8f3cb5a
KangJS <asdfqaz1996@naver.com>
2025-04-21 16:02:10 +0900

    스타일 개선: GlobalBan 컴포넌트의 레이아웃 및 반응형 디자인 조정

--- app/MainPage/components/GlobalBan.js
+++ app/MainPage/components/GlobalBan.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import { fetchGlobalBan } from "@/lib/api/fetchGlobalBan";
+import { useMediaQuery } from "@mui/material";
 
 export default function GlobalBan() {
   const {
@@ -15,19 +16,19 @@ export default function GlobalBan() {
     queryFn: fetchGlobalBan,
   });
 
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   return (
-    <div className="bg-[url('/images/MainPage/globalban-bg.png')] bg-cover bg-center px-80 py-20">
-      <div className="flex flex-col justify-center items-center">
-        <span className="text-[#B217CE] text-[3.125rem] font-[BlackOps]">
-          TODAY
-        </span>
-        <span className="text-[#ffffff] text-[3.125rem] font-[BlackOps]">
+    <div className="bg-[url('/images/MainPage/globalban-bg.png')] 2xl:px-80 xl:px-60 lg:px-40 md:px-20 px-10 bg-cover bg-center py-20">
+      <div className="flex flex-col justify-center items-center sm:text-[3.125rem] text-3xl">
+        <span className="text-[#B217CE] font-[BlackOps]">TODAY</span>
+        <span className="text-[#ffffff] font-[BlackOps] whitespace-nowrap">
           GLOBAL BAN
         </span>
       </div>
 
       {isLoading && (
-        <ul className="flex justify-between gap-6 mt-30 mb-20">
+        <ul className="grid grid-cols-2 md:flex md:justify-between md:gap-6 mt-30 mb-20">
           {[...Array(5)].map((_, idx) => (
             <li
               key={idx}
@@ -40,27 +41,32 @@ export default function GlobalBan() {
       )}
 
       {!isLoading && !isError && (
-        <ul className="flex justify-between gap-6 mt-30 mb-20">
-          {champions.map((champion, idx) => (
-            <li
-              key={idx}
-              className="relative flex flex-col bg-[#1B4A85] w-[10rem] h-[10rem] px-4 pt-4 pb-2 rounded-xl items-center justify-end shadow-lg overflow-visible"
-            >
-              <div className="absolute -top-12 w-[8.75rem] h-[12.5rem] rounded-xl overflow-hidden">
-                <Image
-                  src={`/images/champions/loading/${champion.en_name}.jpg`}
-                  alt={champion.name}
-                  width={140}
-                  height={180}
-                  className="object-center w-full h-full"
-                  priority
-                />
-              </div>
-              <span className="absolute -bottom-10 text-white font-semibold">
-                {champion.name}
-              </span>
-            </li>
-          ))}
+        <ul className="grid grid-cols-2 gap-32 md:flex md:justify-between md:gap-6 mt-30 mb-20 px-auto">
+          {champions.map((champion, idx) => {
+            const imageSrc = isDesktop
+              ? `/images/champions/loading/${champion.en_name}.jpg`
+              : `/images/champions/portrait/${champion.en_name}.png`;
+            return (
+              <li
+                key={idx}
+                className="relative flex flex-col md:bg-[#1B4A85] md:w-[10rem] md:h-[10rem] px-4 pt-4 pb-2 rounded-xl items-center justify-center md:justify-end shadow-lg overflow-visible"
+              >
+                <div className="absolute -top-12 w-[6.25rem] h-[6.25rem] md:w-[8.75rem] md:h-[12.5rem] rounded-xl overflow-hidden items-center justify-center">
+                  <Image
+                    src={imageSrc}
+                    alt={champion.name}
+                    width={!isDesktop ? 100 : 140}
+                    height={!isDesktop ? 100 : 180}
+                    className="object-center"
+                    priority
+                  />
+                </div>
+                <span className="absolute -bottom-14 md:-bottom-10 text-white font-semibold whitespace-nowrap">
+                  {champion.name}
+                </span>
+              </li>
+            );
+          })}
         </ul>
       )}
 
