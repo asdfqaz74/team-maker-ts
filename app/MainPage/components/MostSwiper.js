d845a2396749209584dc1fc9e42f85e4de5ccda5
KangJS <asdfqaz1996@naver.com>
2025-04-22 02:32:10 +0900

    스타일 개선: MostChampion 및 MostSwiper 컴포넌트의 반응형 디자인 적용 및 레이아웃 조정

--- app/MainPage/components/MostSwiper.js
+++ app/MainPage/components/MostSwiper.js
@@ -7,10 +7,13 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import Image from "next/image";
+import useBreakpoint from "@/utils/useBreakpion";
 
 export default function MostSwiper({ champions }) {
   const [activeIndex, setActiveIndex] = useState(0);
   const shiftedChampions = [...champions.slice(1), champions[0]];
+
+  const { is2xl, isxl, islg, ismd, issm } = useBreakpoint();
 
   // 스와이퍼 참초 객체
   const contentSwiperRef = useRef(null);
@@ -37,26 +40,50 @@ export default function MostSwiper({ champions }) {
         loop
         speed={1000}
         slidesPerView={1}
-        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+        onSlideChange={(swiper) => {
+          const realIdx = swiper.realIndex;
+          setActiveIndex(realIdx);
+          bgSwiperRef.current?.slideToLoop(realIdx);
+        }}
         allowTouchMove={false}
         onSwiper={(swiper) => (bgSwiperRef.current = swiper)}
+        observeParents
+        observer
         className="absolute inset-0 opacity-70 z-0 rounded-2xl"
       >
-        {shiftedChampions.map((champion, idx) => {
-          return (
-            <SwiperSlide key={idx}>
-              <div className="relative w-full h-[37.5rem]">
-                <Image
-                  src={champion.logo}
-                  alt={champion.name}
-                  fill
-                  priority
-                  className="object-cover"
-                />
-              </div>
-            </SwiperSlide>
-          );
-        })}
+        {ismd &&
+          shiftedChampions.map((champion, idx) => {
+            return (
+              <SwiperSlide key={idx}>
+                <div className="relative w-full h-[37.5rem]">
+                  <Image
+                    src={champion.logo}
+                    alt={champion.name}
+                    fill
+                    priority
+                    className="object-cover"
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          })}
+
+        {!ismd &&
+          champions.map((champion, idx) => {
+            return (
+              <SwiperSlide key={idx}>
+                <div className="relative w-full h-[37.5rem]">
+                  <Image
+                    src={champion.logo}
+                    alt={champion.name}
+                    fill
+                    priority
+                    className="object-cover"
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          })}
       </Swiper>
 
       {/* 타이틀 */}
@@ -65,7 +92,7 @@ export default function MostSwiper({ champions }) {
         alt="모스트 챔피언 5"
         width={400}
         height={100}
-        className="absolute top-5 left-10 z-20"
+        className="absolute -top-10 md:top-5 md:left-10 z-20"
       />
 
       {/* Swiper 콘텐츠 */}
@@ -78,29 +105,45 @@ export default function MostSwiper({ champions }) {
             prevEl: ".swiper-custom-prev",
           }}
           spaceBetween={30}
-          slidesPerView={4}
+          breakpoints={{
+            1536: { slidesPerView: 4 },
+            1280: { slidesPerView: 3 },
+            768: { slidesPerView: 2 },
+            0: { slidesPerView: 1 },
+          }}
           loop={true}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
-          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+          onSlideChange={(swiper) => {
+            const realIdx = swiper.realIndex;
+            setActiveIndex(realIdx);
+            bgSwiperRef.current?.slideToLoop(realIdx);
+          }}
           speed={1000}
           allowTouchMove={false}
           onSwiper={(swiper) => (contentSwiperRef.current = swiper)}
+          observeParents
+          observer
           className="w-full h-full z-10"
         >
           {champions.map((champion, idx) => {
             const isSecondSlide = idx === (activeIndex + 1) % champions.length;
+            const isFirstSlide = idx === activeIndex % champions.length;
+
+            const sizeClass = ismd
+              ? isSecondSlide
+                ? "w-60 h-90 translate-y-40"
+                : "w-50 aspect-square translate-y-80"
+              : isFirstSlide
+              ? "w-60 h-90 translate-y-40"
+              : "";
 
             return (
               <SwiperSlide key={idx} className="flex justify-center items-end">
                 <div
-                  className={`relative rounded-2xl shadow-lg overflow-visible transition-all duration-300 border border-amber-50 translate-x-15 ${
-                    isSecondSlide
-                      ? "w-60 h-90 translate-y-40"
-                      : "w-50 aspect-square translate-y-80"
-                  }`}
+                  className={`relative rounded-2xl shadow-lg overflow-hidden transition-all duration-300 border border-amber-50 ${sizeClass}`}
                 >
                   <div
-                    className={`absolute w-full h-full rounded-2xl overflow-hidden
+                    className={`absolute w-full h-full rounded-2xl overflow-hidden 
                       }`}
                   >
                     <Image
@@ -114,8 +157,19 @@ export default function MostSwiper({ champions }) {
                     />
                   </div>
                 </div>
-                {isSecondSlide && (
-                  <div className="absolute top-0 -right-[31.25rem]">
+                {ismd && isSecondSlide && (
+                  <div className="absolute top-0 md:-left-72 md:top-32 lg:top-0 lg:left-auto lg:right-12 xl:-right-52 2xl:-right-[31.25rem]">
+                    <div className="flex flex-col gap-4">
+                      <p className="text-4xl font-bold">{champion.name}</p>
+                      <div className="flex gap-4 items-end">
+                        <p className="text-xl font-semibold">총 게임 수</p>
+                        <p>{champion.count} 게임</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {!ismd && isFirstSlide && (
+                  <div className="absolute top-10 bg-gray-900 p-2">
                     <div className="flex flex-col gap-4">
                       <p className="text-4xl font-bold">{champion.name}</p>
                       <div className="flex gap-4 items-end">
@@ -132,29 +186,33 @@ export default function MostSwiper({ champions }) {
       </div>
 
       {/* 커스텀 버튼 */}
-      <button
-        className="swiper-custom-prev absolute -left-20 top-1/2 -translate-y-1/2 z-30 w-[60px] h-[60px] cursor-pointer"
-        onClick={handlePrev}
-      >
-        <Image
-          src="/images/MainPage/Left circle.png"
-          alt="이전 슬라이드"
-          fill
-          className="object-contain"
-        />
-      </button>
+      {ismd && (
+        <>
+          <button
+            className="swiper-custom-prev absolute -left-20 top-1/2 -translate-y-1/2 z-30 w-[60px] h-[60px] cursor-pointer"
+            onClick={handlePrev}
+          >
+            <Image
+              src="/images/MainPage/Left circle.png"
+              alt="이전 슬라이드"
+              fill
+              className="object-contain"
+            />
+          </button>
 
-      <button
-        className="swiper-custom-next absolute -right-20 top-1/2 -translate-y-1/2 z-30 w-[50px] h-[50px] cursor-pointer rotate-180"
-        onClick={handleNext}
-      >
-        <Image
-          src="/images/MainPage/Left circle.png"
-          alt="다음 슬라이드"
-          fill
-          className="object-contain"
-        />
-      </button>
+          <button
+            className="swiper-custom-next absolute -right-20 top-1/2 -translate-y-1/2 z-30 w-[50px] h-[50px] cursor-pointer rotate-180"
+            onClick={handleNext}
+          >
+            <Image
+              src="/images/MainPage/Left circle.png"
+              alt="다음 슬라이드"
+              fill
+              className="object-contain"
+            />
+          </button>
+        </>
+      )}
     </div>
   );
 }
