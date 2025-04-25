a2dccc438690952c70ea30bc8496a340bbaee4bb
KangJS <asdfqaz1996@naver.com>
2025-04-25 17:06:29 +0900

    BanChampion 타입 정의 추가 및 fetchBanChampion 함수의 반환 타입을 Promise<BanChampion[]>로 변경

--- lib/api/fetchBanChampion.ts
+++ lib/api/fetchBanChampion.ts
@@ -1,6 +1,10 @@
-export const fetchBanChampion = async () => {
+import { BanChampion } from "@/types/champion";
+
+export const fetchBanChampion = async (): Promise<BanChampion[]> => {
   const response = await fetch("/api/champion/summary/ban");
   if (!response.ok) throw new Error("챔피언을 불러오는 중 에러 발생");
 
-  return response.json();
+  const data: BanChampion[] = await response.json();
+
+  return data;
 };
