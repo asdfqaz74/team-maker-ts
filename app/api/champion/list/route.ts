e26bf60fd8436ddacd04b857408548c2fbad9e5d
KangJS <asdfqaz1996@naver.com>
2025-04-27 22:24:21 +0900

    ChampionList 및 MongoChampion 타입 정의 개선, GET 함수에서의 반환 타입 명시 및 에러 처리 타입 추가

--- app/api/champion/list/route.ts
+++ app/api/champion/list/route.ts
@@ -1,15 +1,19 @@
 import { connectDB } from "@/lib/mongoose";
 import Champion from "@/models/Champion";
+import { ChampionList, MongoChampion } from "@/types/champion";
 
 export async function GET() {
   await connectDB();
 
   try {
-    const list = await Champion.find({}, { name: 1, en_name: 1 }).sort({
+    const list: MongoChampion[] = await Champion.find(
+      {},
+      { name: 1, en_name: 1 }
+    ).sort({
       name: 1,
     });
 
-    const champions = list.map((champion) => {
+    const champions: ChampionList[] = list.map((champion) => {
       return {
         id: champion._id,
         name: champion.name,
@@ -19,7 +23,7 @@ export async function GET() {
     });
 
     return Response.json(champions, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
     console.error("챔피언을 불러오는 중 에러 발생: ", error);
     return Response.json(
       { error: error.message || "서버 에러가 발생하였습니다." },
--- types/champion.ts
+++ types/champion.ts
@@ -1,16 +1,27 @@
-export interface BanChampion {
+export interface BaseChampion {
   id: string;
   name: string;
   en_name: string;
   image: string;
+}
+
+export interface MongoChampion {
+  _id: string;
+  name: string;
+  en_name: string;
+}
+
+type ConvertMongoId<T> = Omit<T, "_id"> & { id: string };
+
+export interface BanChampion extends BaseChampion {
   count: number;
 }
 
-export interface PickChampion {
-  id: string;
-  name: string;
-  en_name: string;
-  image: string;
+export interface PickChampion extends BaseChampion {
   winRate: number;
   count: number;
+}
+
+export interface ChampionList extends ConvertMongoId<MongoChampion> {
+  image: string;
 }
