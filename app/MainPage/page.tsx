a0d9c963271e84a25da69bf0300d52717547e040
KangJS <asdfqaz1996@naver.com>
2025-04-28 16:19:00 +0900

    Summary 컴포넌트 추가: Pick 컴포넌트를 사용하여 챔피언 요약 정보 표시

--- app/MainPage/page.tsx
+++ app/MainPage/page.tsx
@@ -1,6 +1,6 @@
 import GlobalBan from "./components/GlobalBan";
 import MostChampion from "./components/MostSwiper/MostChampion";
-import Summary from "./components/Summary";
+import Summary from "./components/Summary/Summary";
 
 export default function MainPage() {
   return (
